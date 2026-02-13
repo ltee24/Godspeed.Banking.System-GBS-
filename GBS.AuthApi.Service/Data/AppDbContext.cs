53aab5a47b128e776e10d38978c18b36aafcfb5f
AbdullateefJokanola <ayomidejokanola08@gmail.com>
2026-02-13 17:53:29 +0100

    - configure database and add identity

---
+++ GBS.AuthApi.Service/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GBS.AuthApi.Service.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GBS.AuthApi.Service.Data
+{
+    public class AppDbContext:IdentityDbContext<GodSpeedUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<GodSpeedUser> GodSpeedUsers { get; set; }
+    }
+}
